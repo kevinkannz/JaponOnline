507023c54808fbcf7846af6b0602b552c37b848e
kevin cortes <kevinkannzz@gmail.com>
2021-07-28 10:23:39 -0500

    Se agrego el DOM a js

---
+++ assets/app.js
@@ -0,0 +1,44 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Historia";
+    document.getElementById("banner").src = "img/historia.jpg";
+}
+
+function myFunction2() {
+    document.getElementById("demo").innerHTML = "Cultura";
+    document.getElementById("banner").src = "img/cultura.jpg";
+}
+
+function myFunction3() {
+    document.getElementById("demo").innerHTML = "Turismo";
+    document.getElementById("banner").src = "img/turismo.jpg";
+}
+
+function myFunction4() {
+    document.getElementById("demo").innerHTML = "Gastronomia";
+    document.getElementById("banner").src = "img/gastronomia.jpg";
+}
+
+function myFunction5() {
+    document.getElementById("demo").innerHTML = "Festividades";
+    document.getElementById("banner").src = "img/festividades.jpg";
+}
+
+function myFunction6() {
+    document.getElementById("demo").innerHTML = "Eventos";
+    document.getElementById("banner").src = "img/eventos.jpg";
+}
+
+function myFunction7() {
+    document.getElementById("demo").innerHTML = "Urbanizacion";
+    document.getElementById("banner").src = "img/urbanizacion.jpeg";
+}
+
+function myFunction8() {
+    document.getElementById("demo").innerHTML = "Aportes";
+    document.getElementById("banner").src = "img/aportes.jpg";
+}
+
+function myFunction9() {
+    document.getElementById("demo").innerHTML = "Viajes";
+    document.getElementById("banner").src = "img/viajes.jpg";
+}
