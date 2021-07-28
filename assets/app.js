ac4cb983077ab6e3b3929c9e0953b80b8dc230a6
kevin cortes <kevinkannzz@gmail.com>
2021-07-28 12:09:00 -0500

    modificacion del dom para seccion categorias

--- assets/app.js
+++ assets/app.js
@@ -1,44 +1,53 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Historia";
     document.getElementById("banner").src = "img/historia.jpg";
+    window.location = '#categorias';
 }
 
 function myFunction2() {
     document.getElementById("demo").innerHTML = "Cultura";
     document.getElementById("banner").src = "img/cultura.jpg";
+    window.location = '#categorias';
 }
 
 function myFunction3() {
     document.getElementById("demo").innerHTML = "Turismo";
     document.getElementById("banner").src = "img/turismo.jpg";
+    window.location = '#categorias';
 }
 
 function myFunction4() {
     document.getElementById("demo").innerHTML = "Gastronomia";
     document.getElementById("banner").src = "img/gastronomia.jpg";
+    window.location = '#categorias';
 }
 
 function myFunction5() {
     document.getElementById("demo").innerHTML = "Festividades";
     document.getElementById("banner").src = "img/festividades.jpg";
+    window.location = '#categorias';
 }
 
 function myFunction6() {
     document.getElementById("demo").innerHTML = "Eventos";
     document.getElementById("banner").src = "img/eventos.jpg";
+    window.location = '#categorias';
 }
 
 function myFunction7() {
     document.getElementById("demo").innerHTML = "Urbanizacion";
     document.getElementById("banner").src = "img/urbanizacion.jpeg";
+    window.location = '#categorias';
 }
 
 function myFunction8() {
     document.getElementById("demo").innerHTML = "Aportes";
     document.getElementById("banner").src = "img/aportes.jpg";
+    window.location = '#categorias';
 }
 
 function myFunction9() {
     document.getElementById("demo").innerHTML = "Viajes";
     document.getElementById("banner").src = "img/viajes.jpg";
+    window.location = '#categorias';
 }
